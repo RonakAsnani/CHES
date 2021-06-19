f70c09f14bdab86ee38051e8d8b1bf0965fe6a5d
Pranav <prn2108@gmail.com>
2021-06-19 12:51:19 +0530

    feat: backend setup and setting up connection with mongoDB atlas

--- server/index.js
+++ server/index.js
@@ -8,24 +8,12 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "--------------");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-//   next();
-// });
 
-const PORT = 5000;
-const CONNECTION_URL = "to be added";
-
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log("Server running on port:", PORT))
-  )
+const CONNECTION_URL = 'mongodb+srv://ChesDev:Siphon&Eureka6.2@cluster0.opzrd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((error) => console.log(error.message));
 
-mongoose.set("useFindAndModify", false);
+
+mongoose.set('useFindAndModify', false);
